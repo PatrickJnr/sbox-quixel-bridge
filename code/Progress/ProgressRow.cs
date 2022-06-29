bdc733148c9da380fa51de81e3bd3a3cded3ba85
Patrick Jr <patrickcaulton423@gmail.com>
2022-06-29 18:47:16 +0100

    Revert "Hack fix"
    This reverts commit 0d44e0f3e1633a9099e8dbf677a4b025831b0e71.

--- code/Progress/ProgressRow.cs
+++ code/Progress/ProgressRow.cs
@@ -78,6 +78,57 @@ public class ProgressRow : Widget
 		}
 	}
 
+	protected override void OnPaint()
+	{
+		Paint.Antialiasing = true;
+
+		var rect = LocalRect;
+
+		var color = Theme.Green;
+
+		Paint.ClearPen();
+		Paint.SetBrush( Theme.Grey.WithAlpha( 0.1f ) );
+		var r = rect;
+		r.height = 1;
+		Paint.DrawRect( r );
+
+		Paint.SetPen( color.WithAlpha( 0.3f ) );
+		var iconRect = Paint.DrawMaterialIcon( rect.Contract( 16, 0 ), IconText, 32, TextFlag.LeftCenter );
+
+		var contentRect = LocalRect;
+		contentRect.left += iconRect.right + 16;
+		contentRect.right -= 16.0f;
+
+		if ( cancelButton != null && cancelButton.Visible )
+		{
+			contentRect.right = cancelButton.Position.x - 16.0f;
+		}
+
+		var barRect = contentRect;
+		barRect.top += (contentRect.height / 2.0f);
+		barRect.height = 6.0f;
+		barRect.top -= barRect.height / 2.0f;
+		ProgressBar.Draw( barRect, Progress, color );
+
+		Paint.SetDefaultFont( 8 );
+		Paint.SetPen( Theme.White.WithAlpha( 0.9f ) );
+		Paint.DrawText( contentRect.Contract( 4, 11 ), Title, TextFlag.Left | TextFlag.Top );
+
+		if ( !string.IsNullOrEmpty( BottomRight ) )
+		{
+			Paint.SetDefaultFont( 8 );
+			Paint.SetPen( Theme.White.WithAlpha( 0.7f ) );
+			Paint.DrawText( contentRect.Contract( 4, 11 ), BottomRight, TextFlag.Bottom | TextFlag.Right );
+		}
+
+		if ( !string.IsNullOrEmpty( BottomLeft ) )
+		{
+			Paint.SetDefaultFont( 8 );
+			Paint.SetPen( Theme.White.WithAlpha( 0.7f ) );
+			Paint.DrawText( contentRect.Contract( 4, 11 ), BottomLeft, TextFlag.Bottom | TextFlag.Left );
+		}
+	}
+
 	internal void SetCancel( CancellationTokenSource cancel )
 	{
 		cancelButton.Cursor = CursorShape.Finger;
